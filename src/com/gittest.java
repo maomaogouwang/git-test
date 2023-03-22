9084db18c50e0362efa4b24137613a3bda7d6b03
lk <234260731@qq.com>
2023-03-22 19:18:01 +0800

    second commit

--- src/com/gittest.java
+++ src/com/gittest.java
@@ -2,6 +2,7 @@ package com;
 
 public class gittest {
     public static void main(String[] args){
-        System.out.println("hello git");
+        System.out.println("hello git1");
+        System.out.println("hello git2");
     }
 }
