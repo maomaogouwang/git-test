11c0c952727bb2832e489071a453ccb565cc1e17
lk <234260731@qq.com>
2023-03-22 20:22:17 +0800

    three commit

--- src/com/gittest.java
+++ src/com/gittest.java
@@ -3,8 +3,8 @@ package com;
 public class gittest {
     public static void main(String[] args){
         System.out.println("hello git1");
-        System.out.println("hello git1");
         System.out.println("hello git2");
+        System.out.println("hello git3");
         System.out.println("hello hot-fix");
     }
 }
